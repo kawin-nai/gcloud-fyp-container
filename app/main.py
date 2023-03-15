debffb764680146a40452d41df882e170d30dbfe
qazseee <qazseee@gmail.com>
2023-03-15 23:16:40 +0800

    Added post api v2

--- app/main.py
+++ app/main.py
@@ -45,56 +45,55 @@ def index():
     return 'Welcome to the face recognition API!'
 
 
-@app.route('/verify/<filepath>', methods=['GET'])
-def predict(filepath):
-    try:
-        input_img_path = os.path.join(input_path, filepath)
-        input_embedding = get_embedding(input_img_path, detector, vgg_descriptor, save_to_file=False)
-        # dnnFaceDetector = dlib.cnn_face_detection_model_v1("./app/weights/mmod_human_face_detector.dat")
-        # mmod_embedding = get_embedding_mmod(input_img_path, dnnFaceDetector, vgg_descriptor)
-        # print(mmod_embedding)
-        if input_embedding is None:
-            raise Exception("No face detected in input image")
+# @app.route('/verify/<filepath>', methods=['GET'])
+# def predict(filepath):
+#     try:
+#         input_img_path = os.path.join(input_path, filepath)
+#         input_embedding = get_embedding(input_img_path, detector, vgg_descriptor, save_to_file=False)
+#         # dnnFaceDetector = dlib.cnn_face_detection_model_v1("./app/weights/mmod_human_face_detector.dat")
+#         # mmod_embedding = get_embedding_mmod(input_img_path, dnnFaceDetector, vgg_descriptor)
+#         # print(mmod_embedding)
+#         if input_embedding is None:
+#             raise Exception("No face detected in input image")
+#
+#         all_distance = {}
+#         for persons in os.listdir(verified_path):
+#             # print(persons)
+#             person_distance = []
+#             images = []
+#             for image in os.listdir(os.path.join(verified_path, persons)):
+#                 full_img_path = os.path.join(verified_path, persons, image)
+#                 if full_img_path[-3:] == "jpg":
+#                     images.append(full_img_path)
+#                 # Get embeddings
+#             embeddings = get_embeddings(images, detector, vgg_descriptor, read_from_file=False, save_to_file=False)
+#             if embeddings is None:
+#                 logging.debug("No faces detected")
+#                 continue
+#             # Check if the input face is a match for the known face
+#             # print("input_embedding", input_embedding)
+#             for index, embedding in enumerate(embeddings):
+#                 score = is_match(images[index], embedding, input_embedding)
+#                 person_distance.append(score)
+#             # Calculate the average distance for each person
+#             all_distance[persons] = np.mean(person_distance)
+#         top_ten = sorted(all_distance.items(), key=lambda x: x[1])[:10]
+#         # convert top_ten[0][1] to float
+#         verified = "False"
+#         if float(top_ten[0][1]) < 0.4:
+#             verified = "True"
+#
+#         # Convert float to string
+#         for i in range(len(top_ten)):
+#             top_ten[i] = list(top_ten[i])
+#             top_ten[i][1] = str(top_ten[i][1])
+#             top_ten[i] = tuple(top_ten[i])
+#
+#         return {"message": "Verification Success", "content": top_ten, "verified": verified}, 200
+#     except Exception as e:
+#         return {"message": str(e)}, 400
 
-        all_distance = {}
-        for persons in os.listdir(verified_path):
-            # print(persons)
-            person_distance = []
-            images = []
-            for image in os.listdir(os.path.join(verified_path, persons)):
-                full_img_path = os.path.join(verified_path, persons, image)
-                if full_img_path[-3:] == "jpg":
-                    images.append(full_img_path)
-                # Get embeddings
-            embeddings = get_embeddings(images, detector, vgg_descriptor, read_from_file=False, save_to_file=False)
-            if embeddings is None:
-                logging.debug("No faces detected")
-                continue
-            # Check if the input face is a match for the known face
-            # print("input_embedding", input_embedding)
-            for index, embedding in enumerate(embeddings):
-                score = is_match(images[index], embedding, input_embedding)
-                person_distance.append(score)
-            # Calculate the average distance for each person
-            all_distance[persons] = np.mean(person_distance)
-        top_ten = sorted(all_distance.items(), key=lambda x: x[1])[:10]
-        # convert top_ten[0][1] to float
-        verified = "False"
-        if float(top_ten[0][1]) < 0.4:
-            verified = "True"
 
-        # Convert float to string
-        for i in range(len(top_ten)):
-            top_ten[i] = list(top_ten[i])
-            top_ten[i][1] = str(top_ten[i][1])
-            top_ten[i] = tuple(top_ten[i])
-
-        return {"message": "Verification Success", "content": top_ten, "verified": verified}, 200
-    except Exception as e:
-        return {"message": str(e)}, 400
-
-
-# TODO: Change logic to get representative embedding from db
 @app.route('/verifyfromdb', methods=['GET'])
 def predict_from_db():
     try:
@@ -104,6 +103,75 @@ def predict_from_db():
         input_url = db.collection(u'input_faces').document(u'input').get().to_dict()['image_url']
         logging.info(input_url)
         input_embedding = get_embedding_from_url(input_url, detector, vgg_descriptor, version=2, camera=camera_choice)
+        # input_embedding_senet = get_embedding_from_url(input_url, detector, vgg_descriptor_senet, version=2, camera=camera_choice)
+        if input_embedding is None:
+            raise Exception("No face detected in input image")
+        # Save input embedding to local json file
+        # np.save(os.path.join(input_path, "input_embedding_v2.npy"), input_embedding)
+
+        verified_faces_ref = db.collection(u'verified_faces')
+        verified_faces = verified_faces_ref.stream()
+
+        all_distance = []
+        for person in verified_faces:
+            person_name = person.id
+            person_dict = person.to_dict()
+            person_role = person_dict['role']
+            person_distance = []
+            # person_distance_senet = []
+            person_faces_ref = verified_faces_ref.document(person_name).collection(u'faces')
+            person_faces = person_faces_ref.stream()
+            representative_url = None
+            if 'representative_embedding' in person_dict:
+                representative_embedding = person_dict['representative_embedding']
+                representative_embedding = np.array(representative_embedding)
+                score = is_match("representative", representative_embedding, input_embedding)
+                person_distance.append(score)
+                representative_url = person_dict['representative_url']
+            else:
+                for index, image in enumerate(person_faces):
+                    image_dict = image.to_dict()
+                    if index == 0:
+                        representative_url = image_dict['image_url']
+                    # Get embeddings
+                    resnet_embedding = np.array(image_dict['resnet_embedding'])
+                    # senet_embedding = np.array(image.to_dict()['senet_embedding'])
+                    score = is_match(image_dict['image_name'], resnet_embedding, input_embedding)
+                    # score_senet = is_match(image.to_dict()['image_name'], senet_embedding, input_embedding_senet)
+                    person_distance.append(score)
+                    # person_distance_senet.append(score_senet)
+
+            # Calculate the average distance for each person
+            person_object = dict()
+            person_object['person_name'] = person_name
+            person_object['role'] = person_role
+            person_object['distance'] = np.mean(person_distance)
+            # person_object['distance'] = score
+            person_object['face_url'] = representative_url
+            # person_object['distance_senet'] = np.mean(person_distance_senet)
+            all_distance.append(person_object)
+        print(all_distance)
+        top_ten = sorted(all_distance, key=lambda x: x['distance'])[:10]
+
+        verified = "False"
+        if float(top_ten[0]['distance']) < 0.5:
+            verified = "True"
+
+        return {"message": "Verification Success", "content": top_ten, "verified": verified}, 200
+    except Exception as e:
+        return {"message": str(e)}, 400
+
+
+@app.route('/verifyfrompost', methods=['POST'])
+def predict_from_post():
+    try:
+        # print(request)
+        args = request.args
+        camera_choice = args.get('camera')
+        file = request.files['image']
+        # input_url = db.collection(u'input_faces').document(u'input').get().to_dict()['image_url']
+        # logging.info(input_url)
+        input_embedding = get_embedding_from_post(file, detector, vgg_descriptor, version=2, camera=camera_choice)
         # input_embedding_senet = get_embedding_from_url(input_url, detector, vgg_descriptor_senet, version=2, camera=camera_choice)
         if input_embedding is None:
             raise Exception("No face detected in input image")
